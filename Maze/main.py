9df22441dc8c412689e3e54100822de91ae41a85
Bijunoka <r.lauray@outlook.fr>
2026-03-19 00:26:49 +0100

    Harmonisation de la langue utilisée et ajout de commentaires et docstrings

--- Maze/main.py
+++ Maze/main.py
@@ -1,15 +1,18 @@
 import pygame as pg
-from maze import Labyrinthe
+from maze import Maze
 from ant import Ant
 from viewport import Camera
 
+# Constantes
 WINDOW_WIDTH, WINDOW_HEIGHT = 840, 840
 
+# Initialisation
 pg.init()
-lab = Labyrinthe(5001)
-lab.generer()
+lab = Maze(5001)
+lab.generate()
 ant = Ant(0, 0)
 
+# Window setup
 screen = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pg.display.set_caption("Maze")
 clock = pg.time.Clock()
@@ -21,13 +24,16 @@ cam = Camera(WINDOW_WIDTH, WINDOW_HEIGHT)
 cam.offset_x, cam.offset_y = offset_x, offset_y 
 
 while run:
+    # Events
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
 
+        # Zoom
         if event.type == pg.MOUSEWHEEL:
-            cam.ajuster_zoom(event, lab)
+            cam.adjust_zoom(event, lab)
 
+        # Drag
         if event.type == pg.MOUSEBUTTONDOWN:
             if event.button == 3:
                 is_dragging = True
@@ -38,16 +44,11 @@ while run:
 
     if is_dragging:
         rel_x, rel_y = pg.mouse.get_rel()
-        cam.deplacer(rel_x, rel_y)
+        cam.move(rel_x, rel_y)
 
-    current_time = pg.time.get_ticks()
-    if current_time % 1000 < 500:
-        ant.up(lab)
-    else:
-        ant.down(lab)
-
+    # Draw
     screen.fill((255, 255, 255))
-    cam.dessiner_labyrinthe(screen, lab)
+    cam.draw_maze(screen, lab)
     pg.display.update()
     clock.tick(30)
 
